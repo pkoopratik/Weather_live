7300bfaed863472adf2a5c5c782d30e95b59c2db
gitPK <kalaskarpratiks@gmail.com>
2022-02-03 08:48:50 +0530

    wether app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Temp from "./components/weather/temp";
+import Hed from "./components/weather/hed";
+
+
+
+function App() {
+  return (
+    <>
+    
+   <Hed/>
+    <Temp/>
+    </>
+  );
+}
+
+export default App;
